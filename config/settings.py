d3ea345be52824e3f950ced4cc3c785f272fe87c
vafa <wfa17269@gmail.com>
2026-06-25 18:43:13 +0330

    fix: reorder authentication classes to prioritize JWT over SessionAuthentication
    - Move JWTAuthentication to top of DEFAULT_AUTHENTICATION_CLASSES
    - Prevents CSRF errors when creating new users via JWT
    - SessionAuthentication now only used as fallback

--- config/settings.py
+++ config/settings.py
@@ -175,9 +175,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # --- Django REST Framework (DRF) & JWT Settings ---
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_RENDERER_CLASSES": (
